592719f3a59e48762165107e0e8ee987842586a5
julieilan06400-cloud <julie.ilan06400@hotmail.com>
2026-03-18 17:53:34 +0100

    Implement Telegram bot with message reply functionality

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-print("Mon bot Telegram fonctionne")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salut 👋 je suis ton bot Telegram")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, reply))
+
+app.run_polling()
